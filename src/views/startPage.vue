7d87a6e1b7779fd8e5ede902e43c93aba78f03fd
Kirill <kirill_lavrinenk@mail.ru>
2022-10-23 17:36:09 +0600

    fixL fix youtube

--- src/views/startPage.vue
+++ src/views/startPage.vue
@@ -20,7 +20,7 @@
             <div class="icons">
               <ul class="icons-network">
                 <li class="icons-network__youtube">
-                  <a href="https://youtube.com/c/%D0%95%D0%B3%D0%BE%D1%80%D0%91%D0%B0%D1%80%D0%B0%D0%BD%D0%BE%D0%B2_%D0%BF%D1%81%D0%B8%D1%85%D0%BE%D0%BB%D0%BE%D0%B3" target="_blank">
+                  <a href="https://www.youtube.com/c/%D0%95%D0%B3%D0%BE%D1%80%D0%91%D0%B0%D1%80%D0%B0%D0%BD%D0%BE%D0%B21" target="_blank">
                     <img src="../assets/youtube-white.png"
                       alt="youtube" width="32" height="32">
                   </a>
